4c4f0652b663f9da9aaeb5bf08829862ceda8031
RLOZANO <rigoberto.lozano@siltcorp.com>
2022-06-24 10:32:48 -0600

    Inicio_Proyecto

---
+++ Web_Api_Pedidos/Web_Api_Pedidos/Controllers/ProducController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Web_Api_Pedidos.Datos;
+using Web_Api_Pedidos.Models;
+
+namespace Web_Api_Pedidos.Controllers
+{
+    
+    [Route("api/Producto")]
+    [ApiController]
+    public class ProducController : ControllerBase
+    {
+        /// <summary>
+        /// Devuelve la lista completa de los clientes
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        [Route("Producto/get", Name = "getProducto")]
+        [HttpGet]
+
+        public List<Product> GetResult()
+        {
+            return ProductsData.ListaProd();
+        }
+
+        
+        /// <summary>
+        ///  Busca un Producto por su ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// 
+
+        [Route("Producto/get", Name = "getProducto")]
+        [HttpGet("{id}")]
+
+        public Product Get(int id)
+        {
+            return ProductsData.GetProd(id);
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="oProduc"></param>
+        /// <returns></returns>
+        /// 
+
+        [Route("Producto/post", Name = "postProducto")]
+        [HttpPost]
+
+        public bool agregar(Product oProduc)
+        {
+            return ProductsData.GuardaProducto(oProduc);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="oProduc"></param>
+        /// <returns></returns>
+        /// 
+        [Route("Producto/put", Name = "putProducto")]
+        [HttpPut("{id}")]
+
+        public bool editar(Product oProduc)
+        {
+            return ProductsData.editarCli(oProduc);
+        }
+
+
+        [Route("Producto/del", Name = "delProducto")]
+        [HttpDelete("{id}")]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// 
+        public bool eliminar(int id)
+        {
+            return ProductsData.EliminarProd(id);
+        }
+
+    }
+}
---
+++ Web_Api_Pedidos/Web_Api_Pedidos/Program.cs
@@ -0,0 +1,73 @@
+using FluentAssertions.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using Web_Api_Pedidos.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+
+
+
+
+   
+    
+
+
+
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+
+
+//app.UseHttpsRedirection();
+//app.UseSwagger();
+
+//// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+//// specifying the Swagger JSON endpoint.
+
+
+
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
